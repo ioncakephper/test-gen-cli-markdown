b93292cd63ed5d378504a9768d76cf254975dea6
Ion Gireada <shytiger@yahoo.com>
2024-12-22 14:15:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,10 +12,24 @@ program
     .description('Sample CLI with options and arguments followed by commands with their own arguments and options')
 
 program
+    .argument('[pattern...]', 'Glob filename patterns', ['**/*[Oo]utline.yaml'])
+    .option('-s, --sidebarsFilename <filePath>', 'Filename and path of sidebars filename', 'sidebars.js',
+    .option('--templates <path>', 'Path to templates folder', 'templates')
+    .action((pattern, options) => {})
+    .configureHelp({
+        sortCommands: true,
+        sortOptions: true,
+        helpWidth: 200,
+        globalOptions: true
+    })
+
+program
     .command('generate')
     .alias('g')
     .description('Generate CLI Markdown')
-    .action(() => {
+    .option('--config <filename>', 'Configuration file', 'schema.json')
+    .argument('<filename>', 'Name of file to generate', 'README.md')
+    .action((filename, options) => {
         console.log('Generating CLI Markdown...');
     });
 
